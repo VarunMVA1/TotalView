bf999717e560940f14dabae4c11fec65bd54b8b9
atluri <varun.atl@gmail.com>
2019-05-12 17:26:47 -0400

    Css Changes

--- src/webparts/totalViewWebPart/components/TotalViewWebPart.tsx
+++ src/webparts/totalViewWebPart/components/TotalViewWebPart.tsx
@@ -39,10 +39,8 @@ export default class TotalViewWebPart extends React.Component<ITotalViewWebPartP
         <div className={ styles.totalViewWebPart }>
           <div className={ styles.container }>
             <div className={ styles.row }>
-              <h2 className={ styles.label }>Welcome to SharePoint Framework Webpart with React!</h2>
-            </div>
-            <div className={ styles.row }>
-              <h3 className={ styles.label }>Integration with GITHUB & Azure Devops for CI/CD.</h3>
+              <p><h2 className={ styles.label }>Welcome to SharePoint Framework Webpart with React!</h2></p>
+              <p><h3 className={ styles.label }>Integration with GITHUB & Azure Devops for CI/CD.</h3></p>
             </div>
             <div className={ styles.row }>
               <span className={ styles.label }>Sum of Lists & Libraries</span>
