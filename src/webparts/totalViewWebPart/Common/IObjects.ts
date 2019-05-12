57f4eb2d1444873adc2984d75c62f9754b65556a
atluri <varun.atl@gmail.com>
2019-05-12 16:14:15 -0400

    Space Issue

--- src/webparts/totalViewWebPart/Common/IObjects.ts
+++ src/webparts/totalViewWebPart/Common/IObjects.ts
@@ -1,7 +1,7 @@
 export interface IList{
-    Title?: string,
-    Id?: string,
-    Hidden?: boolean,
-    BaseType?: number,
+    Title?:string,
+    Id?:string,
+    Hidden?:boolean,
+    BaseType?:number,
     BaseTemplate?:number
 }
